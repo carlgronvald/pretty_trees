bcbd1a4394cace4a2e2b96a5715b31e4c64a7834
Carl Frederik Grønvald <carl@gronvald.com>
2021-06-07 12:05:08 +0200

    Absolutely positioned tree

--- Program.fs
+++ Program.fs
@@ -73,33 +73,35 @@ let design tree =
 
 let t = Node('a', [Node('b', [Node('c',[])]); Node('d',[]); Node('e',[Node('f',[])])])
 
+let absolute_positioned_tree postree =
+    let rec helper (Node((label, position), ts)) (translation:float) =
+        (label, position+translation) :: (List.collect (fun t -> helper t (translation+position) ) ts)
+    helper postree 0.0
 
-//let ext (pl:float, pr:float) (q:float) =
-//    (min(pl,q), max(pr,q))
-
-//let pos (Node((label, position:float), children)) =
-//    position
-
-//let child_extents (Node((label, (position:float)), children)) =
-//    List.fold ext (0.0, 0.0) (List.map pos children) 
-
-
-//let generate_postscript tree =
-
+/// Do a breadth first search by level
+/// Returns a list of lists of nodes, where each list is one level.
 let level_bfs tree =
+    // The concept is to keep a list of nodes to be traversed both for the 
+    // current level and the next level. Then, when the current level is 
+    // empty, the current accumulator is appended to the total accumulator
+    // (The current accumulator keeps a list of labels, the total
+    // keeps a list of lists of labels)
     let rec helper current_level next_level acc acc_old =
         match current_level with
         | [] -> //Case where the current level is empty, and we go on to the next level
             match next_level with
-            | [] -> acc::acc_old
-            | _ -> helper next_level [] [] (acc::acc_old)
+            | [] -> acc_old @ [acc]
+            | _ -> helper next_level [] [] (acc_old @ [acc])
         | Node(x, ts)::rest -> //Case where the current level is not empty, so we continue in the current level
-            helper rest (next_level@ts) (x::acc) acc_old
+            helper rest (next_level@ts) (acc @ [x]) acc_old
     helper [tree] [] [] []
 
 let tt = Node('a', [Node('b', [Node('d', []); Node('e', []); Node('f', [])]); Node('c', [])])
 let b2 = level_bfs tt
 printf "level bfs %A" b2
+let rpt = design tt
+let apt = absolute_positioned_tree rpt
+printf "absolutely positioned tree %A" apt
 [<EntryPoint>]
 let main argv =
     printfn ""
